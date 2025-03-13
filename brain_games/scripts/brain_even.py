30a3157f1f0c59f80239ee1c5e37d230aee3b673
Jacobo <jacobo.jaramillo@hotmail.com>
2025-03-13 05:38:24 -0700

    brain_even modificado y terminado deacuerdo al ejercicio

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,17 +4,27 @@ from brain_games.cli import welcome_user
 
 def parity_check(number):
     if number % 2 == 0:
-        return 'Par'
+        return 'yes' # Es par
     else:
-        return 'Impar'
+        return 'no' # No es par, es impar
     
 def play_even_game():
     print("Welcome to the Brain Games!")
-    welcome_user()
+    name=welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    random_number=randint(1,20)
-    print(f'Question: {random_number}')
-    prompt.string('Your answer: ')
-    
-if __name__ == "__main__":
-    play_even_game()
+    for i in range(0,3):    
+        random_number=randint(1,20)
+        print(f'Question: {random_number}')        
+        user_answ=prompt.string('Your answer: ')
+        if user_answ == parity_check(random_number):
+            print('Correct!')
+        else:
+            print(f"'{user_answ}' is wrong answer ;(. Correct answer was '{parity_check(random_number)}'. \nLet's try again, {name}!")
+            break
+        print(f'Congratulations, {name}!')
+
+
+def main():
+    play_even_game()  
+# if __name__ == "__main__":
+#     play_even_game()
