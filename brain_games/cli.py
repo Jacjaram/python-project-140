980d4aaff86e1849dccfecbc66f610f7320d476d
Jacobo <jacobo.jaramillo@hotmail.com>
2025-03-15 10:59:47 -0700

    cli.py Modified con ruff

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,16 @@
 import prompt
 
 def welcome_user():
+
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
 
-def error_message(user,correct,name):
+def error_message(user, correct, name):
+
     print(f"'{user}' is wrong answer ;(. Correct answer was '{correct}'. \nLet's try again, {name}!")
 
 def congratulation_message(name):
+
     print(f'Congratulations, {name}!')
